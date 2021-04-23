461f221fb0c308e818d216483e98162c52849293
AlisherUrazbayev <71267989+AlisherUrazbayev@users.noreply.github.com>
2021-04-23 03:57:25 -0400

    Add files via upload

---
+++ 101173113_comp3133_assig2/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assignmentFullStack';
+  isValid = false;
+
+  ngOnInit(): void {
+    let val = localStorage.getItem('isValidUser')
+
+    if(val != null && val == 'true'){
+        this.isValid = true
+    }else{
+      this.isValid = false
+    }
+  }
+}
